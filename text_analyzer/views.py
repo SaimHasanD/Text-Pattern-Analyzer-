7978b942a8de1b4f95b24b7731423ac331a0f4f9
Saim <saimhasan514@gmail.com>
2026-02-03 20:32:29 +0600

    Reading time calcilator done

--- text_analyzer/views.py
+++ text_analyzer/views.py
@@ -1,4 +1,28 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("Hello Django 👋")
+def reading_time_view(request):
+    minutes = None
+    seconds = None
+    user_text = ""
+    
+    if request.method == 'POST':
+        user_text = request.POST.get('user_text', '')
+        
+        # Split text
+        words = user_text.split()
+        total_words = len(words)
+        
+        # Calculate reading time
+        read_time = total_words / 200
+        
+        # Separate minutes and seconds
+        minutes = int(read_time) 
+        seconds = int((read_time - minutes) * 60) 
+    
+    context = {
+        'minutes': minutes,
+        'seconds': seconds,
+        'user_text': user_text
+    }
+    
+    return render(request, 'text_analyzer/reading_time.html', context)
